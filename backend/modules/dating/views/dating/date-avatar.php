d0082d61e4336077fe07cd1cbbc57ad3d40ce8ac
tqlmm <397114202@qq.com>
2017-03-14 11:29:42 +0800

    fwa

--- backend/modules/dating/views/dating/date-avatar.php
+++ backend/modules/dating/views/dating/date-avatar.php
@@ -15,11 +15,11 @@ $id = Yii::$app->request->get('id');
         <div class="col-md-4 col-md-offset-2">
             <?= \shiyang\webuploader\Cropper::widget() ?>
         </div>
-        <div class="col-md-4">
-            <a id="set-avatar" class="btn btn-success btn-lg" href='<?=Url::toRoute(['/dating/dating/avatar','id'=>$id])?>' onclick="return false;">
+   <!--     <div class="col-md-4">
+            <a id="set-avatar" class="btn btn-success btn-lg" href='<?/*=Url::toRoute(['/dating/dating/avatar','id'=>$id])*/?>' onclick="return false;">
                 系统头像
             </a>
             <div id="avatar-container"></div>
-        </div>
+        </div>-->
     </div>
 </div>
