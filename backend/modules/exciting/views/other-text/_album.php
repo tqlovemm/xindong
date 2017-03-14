27d411baeb532cc8e1d8827f84f971be6595cb07
tqlmm <397114202@qq.com>
2017-03-14 18:05:13 +0800

    fwa

--- backend/modules/exciting/views/other-text/_album.php
+++ backend/modules/exciting/views/other-text/_album.php
@@ -5,10 +5,11 @@ use yii\helpers\Html;
 $website = new \backend\modules\exciting\models\OtherText();
 $albumUrl = Url::toRoute(['/exciting/other-text/view', 'id' => $model['tid']]);
 $src =$website->getCoverPhoto($model['tid']);
+$pre_url = Yii::$app->params['threadimg'];
 ?>
 <div class="album-img">
     <a class="img-thumbnail" href="<?= $albumUrl ?>">
-        <img src="<?= $src ?>" class="album-cover img-responsive" alt="album-cover" style="width: 200px;height: 200px;">
+        <img src="<?= $pre_url.$src ?>" class="album-cover img-responsive" alt="album-cover" style="width: 200px;height: 200px;">
     </a>
 	<p style="padding: 5px;background-color: #fff;"><?= Html::a($model['title'], $albumUrl, ['class' => 'album-name']) ?></p>
 </div>
