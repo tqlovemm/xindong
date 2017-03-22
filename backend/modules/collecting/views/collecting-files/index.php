43d61d0959ef2d84ed351d4e75ac48f5d45c26bd
tqlmm <397114202@qq.com>
2017-03-22 10:39:18 +0800

    fwa

--- backend/modules/collecting/views/collecting-files/index.php
+++ backend/modules/collecting/views/collecting-files/index.php
@@ -15,54 +15,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?php if(in_array(Yii::$app->user->id,[13674])):?>
-        <?= GridView::widget([
-            'dataProvider' => $dataProvider,
-            'filterModel' => $searchModel,
-            'columns' => [
-                'id',
-                'address_province',
-                'address_city',
-                [
-                    'format' => 'raw',
-                    'label' => '发送给17会员的表单一',
-                    'value' => function ($data) {
-                        return 'www.13loveme.com/17-files/'.$data->flag;
-                    }
-
-                ],
-                [
-                    'format' => 'raw',
-                    'label' => '发送给17会员的表单二',
-                    'value' => function ($data) {
-                        return 'www.13loveme.com/17-files/private/'.$data->flag;
-                    }
-
-                ],
-                [
-                    'format' => 'raw',
-                    'label' => '填写情况',
-                    'value' => function ($data) {
-                        if($data->status==0){
-
-                            return '<span style="color:red;">没有填写表单一</span>';
-                        }elseif ($data->status==1){
-                            return '已填写表单一，<span style="color:red;">未填写表单二</span>';
-                        }else{
-
-                            return '表单一二均填写完毕';
-                        }
-
-                    }
-
-                ],
-                'status',
-                'created_at:datetime',
-
-                ['class' => 'yii\grid\ActionColumn', 'template' => '{view}'],
-            ],
-        ]); ?>
-    <?php else:?>
         <?= GridView::widget([
             'dataProvider' => $dataProvider,
             'filterModel' => $searchModel,
@@ -109,7 +61,4 @@ $this->params['breadcrumbs'][] = $this->title;
                 ['class' => 'yii\grid\ActionColumn', 'template' => '{view} {update} {delete}'],
             ],
         ]); ?>
-    <?php endif;?>
-
-
 </div>
