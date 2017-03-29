8f13af9cf73db577ef4999b3d21cb79528c3f13f
tqlmm <397114202@qq.com>
2017-03-29 11:25:44 +0800

    fwa

--- frontend/modules/weixin/controllers/KunSheController.php
+++ frontend/modules/weixin/controllers/KunSheController.php
@@ -281,7 +281,7 @@ class KunSheController extends Controller
                 $EventKey = $this->postObj->EventKey;
                 if($EventKey=='V1001_GOOD'){
                     $media_id = 'O6eLs8-Y0QQbm638hdjTsn4VEnrtMwU25BtI2_ec8Z8';
-                    $this->text($media_id);
+                    $this->image($media_id);
                     exit;
                 }elseif($EventKey=='V1002_GOOD'){
                     $media_id = 'O6eLs8-Y0QQbm638hdjTsp2XzF2JsEc1rSJyze5CyBM';
