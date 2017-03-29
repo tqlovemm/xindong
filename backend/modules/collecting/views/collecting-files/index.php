46481beec8c9aee461af02036fb6f9774b7bfd36
tqlmm <397114202@qq.com>
2017-03-29 12:31:54 +0800

    fwa

--- backend/modules/collecting/views/collecting-files/index.php
+++ backend/modules/collecting/views/collecting-files/index.php
@@ -21,43 +21,12 @@ $this->params['breadcrumbs'][] = $this->title;
             'columns' => [
                 'id',
                 'address_province',
+                'weichat',
+                'cellphone',
+                'address_province',
                 'address_city',
-                [
-                    'format' => 'raw',
-                    'label' => '发送给17会员的表单一',
-                    'value' => function ($data) {
-                        return 'www.13loveme.com/17-files/'.$data->flag;
-                    }
-
-                ],
-                [
-                    'format' => 'raw',
-                    'label' => '发送给17会员的表单二',
-                    'value' => function ($data) {
-                        return 'www.13loveme.com/17-files/private/'.$data->flag;
-                    }
-
-                ],
-                [
-                    'format' => 'raw',
-                    'label' => '填写情况',
-                    'value' => function ($data) {
-                        if($data->status==0){
-
-                            return '<span style="color:red;">没有填写表单一</span>';
-                        }elseif ($data->status==1){
-                            return '已填写表单一，<span style="color:red;">未填写表单二</span>';
-                        }else{
-
-                            return '表单一二均填写完毕';
-                        }
-
-                    }
-
-                ],
                 'status',
                 'created_at:datetime',
-
                 ['class' => 'yii\grid\ActionColumn', 'template' => '{view} {update} {delete}'],
             ],
         ]); ?>
