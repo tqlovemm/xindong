ee7aabf83a22d78e667a1c5d5c49a9d36cb69080
tqlmm <397114202@qq.com>
2017-03-30 14:48:17 +0800

    fwa

--- frontend/modules/bgadmin/views/seventeen-man/judge-vip.php
+++ frontend/modules/bgadmin/views/seventeen-man/judge-vip.php
@@ -6,6 +6,6 @@ $this->title = "客服微信号";
     <h4>您的编号：<?=$userModel->id?></h4>
     <p>对不起您还未开通报名权限或您的权限已到期，请长按扫描添加下面的二维码添加客服微信号，将您的编号告知客服开通</p>
     <div class="row" style="margin: 0;">
-        <img class="img-responsive" src="/images/584113871864559426.jpg">
+        <img class="img-responsive" src="/images/353458547328504766.jpg">
     </div>
 </div>
