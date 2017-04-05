f6ad5daf5f9206640f354fab7f22c7d38b8b0fc8
tqlmm <397114202@qq.com>
2017-04-05 15:43:54 +0800

    fwa

--- backend/modules/app/controllers/AppOrderController.php
+++ backend/modules/app/controllers/AppOrderController.php
@@ -49,9 +49,10 @@ class AppOrderController extends Controller
         $model_3 = AppOrderList::find()->select('count(*) as n,sum(total_fee) as t,month_time as w,max(updated_at) as max,min(updated_at) as min')->groupBy('month_time')->orderBy('month_time desc')->asArray()->all();
         $model_4 = AppOrderList::find()->select('count(*) as n,sum(total_fee) as t,max(updated_at) as max,min(updated_at) as min')->asArray()->all();
         $model_5 = AppOrderList::find()->select('count(*) as n,sum(total_fee) as t,max(updated_at) as max,min(updated_at) as min,channel')->groupBy('channel')->asArray()->all();
+        $model_6 = AppOrderList::find()->select('count(*) as n,sum(total_fee) as t,created_at as c')->groupBy('created_at')->orderBy('created_at desc')->asArray()->all();
 /*      echo "<pre>";
         return var_dump($model_2);*/
-        return $this->render('static',['model_1'=>$model_1,'model_2'=>$model_2,'model_3'=>$model_3,'model_4'=>$model_4,'model_5'=>$model_5,]);
+        return $this->render('static',['model_1'=>$model_1,'model_2'=>$model_2,'model_3'=>$model_3,'model_4'=>$model_4,'model_5'=>$model_5,'model_6'=>$model_6,]);
     }
 
     /**
--- backend/modules/app/views/app-order/static.php
+++ backend/modules/app/views/app-order/static.php
@@ -1,12 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2017/4/5
- * Time: 14:02
- */
-$this->title = "app节操币消费统计";
-
+    $this->title = "app节操币消费统计";
 ?>
 <div class="static-index">
     <div class="row">
@@ -44,16 +37,7 @@ $this->title = "app节操币消费统计";
         </div>
     </div>
     <div class="row">
-        <div class="col-md-6">
-            <table class="table table-bordered">
-                <caption>每周统计</caption>
-                <tr class="warning"><th>序号</th><th>周时间</th><th>总金额</th><th>单数</th><th>时间</th></tr>
-                <?php foreach ($model_2 as $key=>$item):?>
-                <tr class="success"><td><?=$key+1?></td><td>第<?=date('W',$item['w'])?>周</td><td><?=$item['t']?>元</td><td><?=$item['n']?>笔</td><td>从 <?=date('Y年m月d日',$item['min'])?> 到 <?=date('Y年m月d日',$item['max'])?></td></tr>
-                <?php endforeach;?>
-            </table>
-        </div>
-        <div class="col-md-6">
+        <div class="col-md-6" style="height: 800px;overflow:auto;">
             <table class="table table-bordered">
                 <caption>每月统计</caption>
                 <tr class="warning"><th>序号</th><th>月时间</th><th>总金额</th><th>单数</th><th>时间</th></tr>
@@ -67,14 +51,23 @@ $this->title = "app节操币消费统计";
                     <tr class="success"><td><?=$key+1?></td><td><?=$mouth_txt?></td><td><?=$item['t']?>元</td><td><?=$item['n']?>笔</td><td>从 <?=date('Y年m月d日',$item['min'])?> 到 <?=date('Y年m月d日',$item['max'])?></td></tr>
                 <?php endforeach;?>
             </table>
+            <table class="table table-bordered">
+                <caption>每周统计</caption>
+                <tr class="warning"><th>序号</th><th>周时间</th><th>总金额</th><th>单数</th><th>时间</th></tr>
+                <?php foreach ($model_2 as $key=>$item):?>
+                    <tr class="success"><td><?=$key+1?></td><td>第<?=date('W',$item['w'])?>周</td><td><?=$item['t']?>元</td><td><?=$item['n']?>笔</td><td>从 <?=date('Y年m月d日',$item['min'])?> 到 <?=date('Y年m月d日',$item['max'])?></td></tr>
+                <?php endforeach;?>
+            </table>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-6"  style="height: 800px;overflow:auto;">
+            <table class="table table-bordered">
+                <caption>每日统计</caption>
+                <tr class="warning"><th>序号</th><th>时间</th><th>总金额</th><th>单数</th></tr>
+                <?php foreach ($model_6 as $key=>$item):?>
+                    <tr class="success"><td><?=$key+1?></td><td><?=date('Y-m-d',$item['c'])?></td><td><?=$item['t']?>元</td><td><?=$item['n']?>笔</td></tr>
+                <?php endforeach;?>
+            </table>
+        </div>
 
-        </div>
-        <div class="col-md-6">
-
-        </div>
     </div>
 </div>
