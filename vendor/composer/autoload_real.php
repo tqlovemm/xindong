30b0062efb2d496cf71b3b61bcd9007f9910cdb1
tqlmm <397114202@qq.com>
2017-04-12 11:12:33 +0800

    uhya

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,27 +22,22 @@ class ComposerAutoloaderInitb188f018e94fdef38c03bed878d18e4e
         spl_autoload_register(array('ComposerAutoloaderInitb188f018e94fdef38c03bed878d18e4e', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitb188f018e94fdef38c03bed878d18e4e', 'loadClassLoader'));
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
-        if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
-            call_user_func(\Composer\Autoload\ComposerAutoloaderInitb188f018e94fdef38c03bed878d18e4e::getInitializer($loader));
 
-        }else{
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
+        $map = require __DIR__ . '/autoload_namespaces.php';
+        foreach ($map as $namespace => $path) {
+            $loader->set($namespace, $path);
         }
+
+        $map = require __DIR__ . '/autoload_psr4.php';
+        foreach ($map as $namespace => $path) {
+            $loader->setPsr4($namespace, $path);
+        }
+
+        $classMap = require __DIR__ . '/autoload_classmap.php';
+        if ($classMap) {
+            $loader->addClassMap($classMap);
+        }
+
 
         $loader->register(true);
 
