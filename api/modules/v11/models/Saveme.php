1ecb7be4f78ef247698433db100d61415dcd7691
tqlmm <397114202@qq.com>
2017-04-14 15:38:59 +0800

    uhya

--- api/modules/v11/models/Saveme.php
+++ api/modules/v11/models/Saveme.php
@@ -31,16 +31,16 @@ class Saveme extends ActiveRecord
     public function rules()
     {
         return [
-            [['created_id', 'address_id', 'content', 'end_time', 'status'], 'required'],
-            [['created_id', 'address_id', 'created_at', 'end_time', 'status','updated_at'], 'integer'],
-            [['content'], 'string', 'max' => 255]
+            [['created_id', 'address', 'content', 'end_time', 'status'], 'required'],
+            [['created_id', 'created_at', 'end_time', 'status','updated_at'], 'integer'],
+            [['content', 'address'], 'string', 'max' => 255]
         ];
     }
 
     public function fields(){
 
         return [
-            'id','created_id', 'content', 'created_at','end_time', 'status', 'prov_name','photos',
+            'id','created_id', 'address', 'content', 'created_at','end_time', 'status', 'photos',
         ];
     }
 
@@ -52,19 +52,12 @@ class Saveme extends ActiveRecord
         return [
             'id' => 'ID',
             'created_id' => '发布人',
-            'address_id' => '地点id',
+            'address' => '地点',
             'content' => '要求',
             'end_time' => '截止时间',
             'updated_at' => 'updated_at',
             'status' => '状态',
         ];
-    }
-
-    public function getprov_name(){
-
-        $province = Yii::$app->db->createCommand("select prov_name from {{%province}} where prov_id=$this->address_id")->queryOne();
-
-        return $province['prov_name'];
     }
 
     public function getPhotos(){
