1ffc0f8ade4bbc46153412028f55916eafd376ec
tqlmm <397114202@qq.com>
2017-04-26 15:38:29 +0800

    uhya

--- backend/modules/financial/views/financial-wechat/past-join-record.php
+++ backend/modules/financial/views/financial-wechat/past-join-record.php
@@ -4,7 +4,7 @@ $this->title = $wechat." 每日人数统计";
 $this->registerCssFile('@web/js/lightbox/css/lightbox.css');
 $this->registerJsFile('@web/js/lightbox/js/lightbox.min.js', ['depends' => ['yii\web\JqueryAsset'], 'position' => \yii\web\View::POS_END]);
 $this->registerCss("
-    .table thead tr{background-color: #ddd;}
+    .table thead tr{background-color: #eee;}
     .table thead tr th{border:1px solid #fff;text-align:center;vertical-align: middle;}
     .table tr td{border:1px solid #eee;text-align:center;}
     .follow{margin-bottom:0;}
