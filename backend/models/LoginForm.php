74002e588fbfe406fc8c8ea79967ab2e0bd7bb76
tqlmm <397114202@qq.com>
2017-05-02 13:13:31 +0800

    Merge remote-tracking branch 'remotes/origin/test'
    # Conflicts:
    #	frontend/controllers/SiteController.php

--- backend/models/LoginForm.php
+++ backend/models/LoginForm.php
@@ -56,7 +56,7 @@ class LoginForm extends Model
         $save_code = $session->get('code');
         $save_mobile = $session->get('mobile');
 
-        if($this->verification!=$save_code){
+        if($this->verification!="$save_code"){
             return $this->addError('verification','验证码错误');
         }
         if($this->username!=$save_mobile){
