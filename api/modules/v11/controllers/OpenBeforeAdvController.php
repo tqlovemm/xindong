521803ca5e5a777e916becb24e136ecff341bb94
tqlmm <397114202@qq.com>
2017-05-08 12:28:02 +0800

    Merge remote-tracking branch 'remotes/origin/test'
    # Conflicts:
    #	frontend/controllers/SiteController.php

--- api/modules/v11/controllers/OpenBeforeAdvController.php
+++ api/modules/v11/controllers/OpenBeforeAdvController.php
@@ -1,6 +1,7 @@
 <?php
 namespace api\modules\v11\controllers;
 
+use api\modules\v11\models\OpenBeforeAdv;
 use yii;
 use yii\rest\ActiveController;
 use api\components\CsvDataProvider;
@@ -37,15 +38,36 @@ class OpenBeforeAdvController extends ActiveController {
         return $action;
     }
 
-
+    /**
+     * @return mixed
+     * get
+     * v11/open-before-advs?access-token={cid}
+     *{
+    "code": "200",
+    "message": "success",
+    "data": {
+    "adv_id": 1,
+    "content": "hhh",
+    "openUrl": "http://www.13loveme.com",
+    "contentSize": "1251*2540",
+    "duration": 5,
+    "status": 10
+    }
+    }
+     *
+     * {
+    "code": "203",
+    "message": "fail",
+    "data": "当前没有可展示广告"
+    }
+     */
     public function actionIndex() {
     	$model = $this->modelClass;
         $query = $model::findOne(['status'=>10]);
         if(empty($query)){
-
-            yii\myhelper\Response::show('203','当前没有可展示广告','当前没有可展示广告');
+            yii\myhelper\Response::show('203','fail','当前没有可展示广告');
         }
-        return $query;
+        yii\myhelper\Response::show('200','success',$query->getAttributes());
     }
 
     protected function findModel($id)
