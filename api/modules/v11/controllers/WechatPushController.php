696191bf64688a9510e0202fbf37bec3d2818e85
tqlmm <397114202@qq.com>
2017-05-11 15:50:41 +0800

    Merge remote-tracking branch 'remotes/origin/test'
    # Conflicts:
    #	frontend/controllers/SiteController.php

---
+++ api/modules/v11/controllers/WechatPushController.php
@@ -0,0 +1,67 @@
+<?php
+namespace api\modules\v11\controllers;
+
+use yii;
+use yii\helpers\Response;
+use yii\rest\ActiveController;
+use api\components\CsvDataProvider;
+use yii\web\NotFoundHttpException;
+
+
+class WechatPushController extends ActiveController {
+
+    public $modelClass = 'api\modules\v11\models\WechatPush';
+    public $serializer = [
+        'class' => 'app\components\Serializer',
+        'collectionEnvelope' => 'data',
+    ];
+    public function behaviors() {
+        $behaviors = parent::behaviors();
+        return $behaviors;
+    }
+
+    public function actions() {
+        $action = parent::actions();
+        unset($action['index'], $action['view'], $action['create'], $action['update'], $action['delete']);
+        return $action;
+    }
+
+    public function actionCreate() {
+
+    	$model = new $this->modelClass();
+    	$model->load(Yii::$app->request->getBodyParams(), '');
+        if (!$model->save()) {
+            Response::show('201',array_values($model->getFirstErrors())[0], $model->getFirstErrors());
+        }else{
+            Response::show('200','ok');
+        }
+
+
+    }
+
+    public function actionView($id) {
+
+        $model = $this->findModel($id);
+        return $model;
+    }
+
+    public function actionDelete($id)
+    {
+       if($this->findModel($id)->delete()){
+           Response::show('200','ok');
+        }
+    }
+
+    protected function findModel($id)
+    {
+        $modelClass = $this->modelClass;
+
+        if (($model = $modelClass::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new yii\web\NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+
+}
+
