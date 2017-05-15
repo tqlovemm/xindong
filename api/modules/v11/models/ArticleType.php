4113635f10a82a22e09a33da41d68d319c290090
xindong <1176306824@qq.com>
2017-05-15 17:55:12 +0800

    articletype

---
+++ api/modules/v11/models/ArticleType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace api\modules\v11\models;
+
+use Yii;
+use app\components\db\ActiveRecord;
+
+/**
+ * This is the model class for table "pre_article_type".
+ *
+ * @property integer $tid
+ * @property string $typename
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property integer $status
+ */
+class ArticleType extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'pre_article_type';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['typename', 'created_at', 'updated_at', 'status'], 'required'],
+            [['created_at', 'updated_at', 'status'], 'integer'],
+            [['typename'], 'string', 'max' => 100]
+        ];
+    }
+
+    public function fields(){
+        return [
+            'typename',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'tid' => 'Tid',
+            'typename' => 'Typename',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'status' => 'Status',
+        ];
+    }
+}
