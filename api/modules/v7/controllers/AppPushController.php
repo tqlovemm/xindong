fc03134530a92b2f3b14e2ced5d6b658a2454117
tqlmm <397114202@qq.com>
2017-05-16 10:30:50 +0800

    Merge branch 'master' into test
    # Conflicts:
    #	api/modules/v11/models/FormThreadComments.php
    #	api/modules/v11/models/FormThreadThumbsUp.php

--- api/modules/v7/controllers/AppPushController.php
+++ api/modules/v7/controllers/AppPushController.php
@@ -49,7 +49,7 @@ class AppPushController extends ActiveController
             $cid = $id;
             $userModel = User::findOne(['cid'=>$cid]);
             $uid = $userModel->id;
-            $query['unread_thread_count'] = Message::find()->where(['to_id'=>$uid,'is_read'=>1])->count();
+            $query['unread_thread_count'] = (int)Message::find()->where(['to_id'=>$uid,'is_read'=>1])->count();
         }else{
             $userModel = User::findOne($id);
             $cid = $userModel->cid;
