db17bef27580ae135ab3c64acddefbdf02c8a378
tqlmm <397114202@qq.com>
2017-05-16 09:58:34 +0800

    Merge branch 'master' into test
    # Conflicts:
    #	api/modules/v11/models/FormThreadComments.php
    #	api/modules/v11/models/FormThreadThumbsUp.php

--- api/modules/v7/controllers/AppPushController.php
+++ api/modules/v7/controllers/AppPushController.php
@@ -45,17 +45,17 @@ class AppPushController extends ActiveController
 
     public function actionView($id){
         if(strlen($id)>10){
-            $userModel = User::findOne(['cid'=>$id]);
             $cid = $id;
-            $uid = $userModel->id;
+            $query['unread_thread_count'] = AppPush::find()->select('count(*) as count,type')->where(['cid'=>$cid,'is_read'=>1])->andWhere('type=SSCOMM_NEWSCOMMENT_DETAIL')->count();
         }else{
             $userModel = User::findOne($id);
             $cid = $userModel->cid;
             $uid = $id;
+            $query['unread_thread_count'] = (int)FormThreadPushMsg::find()->where(['user_id'=>$uid,'read_user'=>0])->count();
         }
-        $pushModel = ArrayHelper::map(AppPush::find()->select('count(*) as count,type')->where(['cid'=>$cid,'is_read'=>1])->groupBy('type')->asArray()->all(),'type','count');
 
-        $query['unread_thread_count'] = (int)FormThreadPushMsg::find()->where(['user_id'=>$uid,'read_user'=>0])->count();
+        $pushModel = ArrayHelper::map(AppPush::find()->select('count(*) as count,type')->where(['cid'=>$cid,'is_read'=>1])->andWhere(('type!=SSCOMM_NEWSCOMMENT_DETAIL'))->groupBy('type')->asArray()->all(),'type','count');
+
         $query['other_saveme_count'] = isset($pushModel['SSCOMM_SAVEME'])?$pushModel['SSCOMM_SAVEME']:0;
         $query['other_message_count'] = array_sum($pushModel)-$query['other_saveme_count'];
         $query['unread_count'] = array_sum($query);
