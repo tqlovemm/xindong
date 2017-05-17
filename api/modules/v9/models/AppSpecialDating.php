c77beda2586eb8177300a631c0a0d4d2877b9e0c
tqlmm <397114202@qq.com>
2017-05-17 11:32:59 +0800

    Merge branch 'master' into test
    # Conflicts:
    #	api/modules/v11/models/FormThreadComments.php
    #	api/modules/v11/models/FormThreadThumbsUp.php

--- api/modules/v9/models/AppSpecialDating.php
+++ api/modules/v9/models/AppSpecialDating.php
@@ -78,7 +78,7 @@ class AppSpecialDating extends \yii\db\ActiveRecord
     public function fields()
     {
         return [
-            'coin', 'limit_count', 'limit_vip', 'created_at', 'updated_at', 'status', 'sign_up_count', 'introduce',
+            'zid','coin', 'limit_count', 'limit_vip', 'created_at', 'updated_at', 'status', 'sign_up_count', 'introduce',
             'p_info', 'h_info','address', 'address_detail',
             'tag_type'=>function(){
                 if($this->tag_type==1){
