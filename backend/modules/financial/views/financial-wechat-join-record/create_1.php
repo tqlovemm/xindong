5a5d1b88d01ad09801c71f40aead8b60d0d69051
tqlmm <397114202@qq.com>
2017-05-18 11:39:37 +0800

    Merge branch 'master' into test
    # Conflicts:
    #	api/modules/v11/models/FormThreadComments.php
    #	api/modules/v11/models/FormThreadThumbsUp.php

--- backend/modules/financial/views/financial-wechat-join-record/create_1.php
+++ backend/modules/financial/views/financial-wechat-join-record/create_1.php
@@ -15,7 +15,7 @@ $this->title = '会员入会收款记录';
     <?= $form->field($model, 'vip')->textInput(['maxlength' => true])->label('会员等级')  ?>
     <?= $form->field($model, 'number')->textInput(['maxlength' => true])->label('会员编号')  ?>
     <?= $form->field($model, 'join_source')->dropDownList([
-        '微博'=>'微博','微信公众号'=>'微信公众号','知乎'=>'知乎','Twitter'=>'Twitter',
+        '微博'=>'微博','微信公众号'=>'微信公众号','知乎'=>'知乎','Twitter'=>'Twitter','Tumblr'=>'Tumblr',
         'Facebook'=>'Facebook','Instagram'=>'Instagram','朋友推荐'=>'朋友推荐',
         '微信朋友圈'=>'微信朋友圈','官网'=>'官网','APP'=>'APP','其他'=>'其他'])->label('入会来源')  ?>
     <?= $form->field($model, 'channel')->dropDownList(['微信'=>'微信','支付宝'=>'支付宝','淘宝'=>'淘宝','公司银行卡转账'=>'公司银行卡转账','海外代充'=>'海外代充','PayPal'=>'PayPal'])->label('付款渠道')  ?>
