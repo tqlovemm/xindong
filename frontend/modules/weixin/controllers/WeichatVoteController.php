727ff42f0e7160ff58e363c558822d2ad7cbe402
tqlmm <397114202@qq.com>
2017-05-24 14:09:38 +0800

    Merge branch 'master' into test
    # Conflicts:
    #	api/modules/v11/models/FormThreadComments.php
    #	api/modules/v11/models/FormThreadThumbsUp.php

--- frontend/modules/weixin/controllers/WeichatVoteController.php
+++ frontend/modules/weixin/controllers/WeichatVoteController.php
@@ -73,24 +73,20 @@ class WeichatVoteController extends Controller
             $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid={$this->options['appid']}&secret={$this->options['appsecret']}&code={$data['code']}&grant_type=authorization_code";
 
             $access = file_get_contents($url);
-
             $result = json_decode($access);
-
-            $access_token = $result->access_token;
             $token = (new AccessToken())->getAccessToken();
             $openid = $result->openid;
 
-            $url2 = "https://api.weixin.qq.com/sns/userinfo?access_token={$access_token}&openid={$openid}&lang=zh_CN";
             $url3 = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$token&openid=$openid";
-            $userInfo = json_decode(file_get_contents($url2),true);
-            $subject = json_decode(file_get_contents($url3),true);
+
+            $userInfo = json_decode(file_get_contents($url3),true);
 
             $this->addCookie('vote_01_openid',$userInfo['openid']);
             $this->addCookie('vote_01_headimgurl',$userInfo['headimgurl']);
             $this->addCookie('vote_01_nickname',$userInfo['nickname']);
 
             $voteUrl = '/weixin/weichat-vote/vote-man';//投票地址
-            return var_dump($subject);
+            return var_dump($userInfo);
 
             return $this->redirect($voteUrl);
         }
