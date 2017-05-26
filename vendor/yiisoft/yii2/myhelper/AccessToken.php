2ab4d74308ceb124e22ff58eb313f2f2b75145c9
tqlmm <397114202@qq.com>
2017-05-26 16:16:17 +0800

    Merge branch 'master' into test
    # Conflicts:
    #	api/modules/v11/models/FormThreadComments.php
    #	api/modules/v11/models/FormThreadThumbsUp.php

--- vendor/yiisoft/yii2/myhelper/AccessToken.php
+++ vendor/yiisoft/yii2/myhelper/AccessToken.php
@@ -29,14 +29,14 @@ class AccessToken
         return $access_token;
     }
     public function getAccessTokenKs() {
-        $this->cache = Yii::$app->cache;
-        $data = $this->cache->get('access_token_ks');
+        $cache = Yii::$app->cache;
+        $data = $cache->get('access_token_ks');
         if (empty($data)) {
             $token_access_url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" . Yii::$app->params['id_ks'] . "&secret=" . Yii::$app->params['secret_ks'];
             $res = json_decode($this->getData($token_access_url));
             $access_token = $res->access_token;
             if ($access_token) {
-                $this->cache->set('access_token_ks',$access_token,7000);
+                $cache->set('access_token_ks',$access_token,7000);
             }
         } else {
             $access_token = $data;
