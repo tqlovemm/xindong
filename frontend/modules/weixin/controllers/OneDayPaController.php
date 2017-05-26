9a669ddc6b8c133aa7c361157d88c341d405471a
tqlmm <397114202@qq.com>
2017-05-26 15:25:01 +0800

    Merge branch 'master' into test
    # Conflicts:
    #	api/modules/v11/models/FormThreadComments.php
    #	api/modules/v11/models/FormThreadThumbsUp.php

---
+++ frontend/modules/weixin/controllers/OneDayPaController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace frontend\modules\weixin\controllers;
+use yii\web\Controller;
+use Yii;
+use yii\myhelper\AccessToken;
+class OneDayPaController extends Controller
+{
+    public function actionIndex()
+    {
+        $callback = "http://13loveme.com/weixin/one-day-pa/wei-user";
+        $this->getCode($callback);
+    }
+
+    protected function getCode($callback){
+        $callback = urlencode($callback);
+        $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" . Yii::$app->params['app_id_ks'] . "&redirect_uri={$callback}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+        return $this->redirect($url);
+
+    }
+
+    public function actionWeiUser(){
+
+        $data['code'] = Yii::$app->request->get('code');
+        $data['state'] = Yii::$app->request->get('state');
+
+        if(!empty($data['code'])){
+
+            $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" . Yii::$app->params['app_id_ks'] . "&secret=" . Yii::$app->params['app_secret_ks'] . "&code={$data['code']}&grant_type=authorization_code";
+
+            $access = file_get_contents($url);
+            $result = json_decode($access);
+            $token = (new AccessToken())->getAccessToken();
+            $openid = $result->openid;
+
+            $url3 = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$token&openid=$openid";
+
+            $userInfo = json_decode(file_get_contents($url3),true);
+
+            if(isset($userInfo['errcode'])&&$userInfo['errcode']==40001){
+                Yii::$app->cache->delete('access_token_js');
+                return $this->redirect('index');
+            }
+
+           // $this->addCookie('userweinfo',json_encode($userInfo));
+
+           // $voteUrl = '/weixin/weichat-vote/vote-man';//投票地址
+
+            return var_dump($userInfo);
+            //return $this->redirect($voteUrl);
+        }else{
+            return $this->redirect('index');
+        }
+
+    }
+
+
+}
--- vendor/yiisoft/yii2/myhelper/AccessToken.php
+++ vendor/yiisoft/yii2/myhelper/AccessToken.php
@@ -99,5 +99,43 @@ class AccessToken
         return $tmpInfo;
     }
 
+    /**
+     * @param $openid
+     * @return mixed
+     * 获取用户信息
+     */
+    public static function getUserInfo($openid){
 
+        $token = self::getAccessToken();
+        $url3 = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$token&openid=$openid";
+        $userInfo = json_decode(file_get_contents($url3),true);
+        if(isset($userInfo['errcode'])&&$userInfo['errcode']==40001){
+            Yii::$app->cache->delete('access_token');
+            self::getUserInfo($openid);
+        }
+        return $userInfo;
+    }
+
+    /**
+     * @param $cookie_name
+     * @param $cookie_value
+     * 添加cookie
+     */
+    public static function addCookie($cookie_name,$cookie_value){
+
+        $cookies = \Yii::$app->response->cookies;
+        $cookie = \Yii::$app->request->cookies;
+        if(empty($cookie->get($cookie_name))){
+            $cookies->add(new \yii\web\Cookie([
+                'name' => $cookie_name,
+                'value' => $cookie_value,
+                'expire'=>time()+3600*24*7,
+            ]));
+        }
+    }
+
+    public static function getCookie($cookie_name){
+        $cookie = \Yii::$app->request->cookies;
+        return $cookie->getValue($cookie_name);
+    }
 }
