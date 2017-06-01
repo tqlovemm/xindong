4476dfd1d65f5f20db19ae82bee303375a7707fb
xindong <1176306824@qq.com>
2017-06-01 17:07:20 +0800

    article

--- api/modules/v11/models/ArticleCollection.php
+++ api/modules/v11/models/ArticleCollection.php
@@ -8,6 +8,8 @@ use app\components\db\ActiveRecord;
 class ArticleCollection extends ActiveRecord
 {
     public $_article;
+    public $_like;
+    public $_islike;
     /**
      * @inheritdoc
      */
@@ -44,8 +46,14 @@ class ArticleCollection extends ActiveRecord
 
     public function fields(){
         $this->_article = Yii::$app->db->createCommand("select id,title,miaoshu,wimg,created_at from {{%article}} where id=$this->aid")->queryOne();
+        $this->_like = Yii::$app->db->createCommand("select userid from {{%article_like}} where aid=$this->aid")->queryAll();
+        for($i=0;$i<count($this->_like);$i++){
+            if($this->_like[$i]['userid'] == $this->userid){
+                $this->_islike = 1;
+            }
+        }
         return [
-            'id','article_id'=>function(){return $this->_article['id'];},'title'=>function(){return $this->_article['title'];},'miaoshu'=>function(){return $this->_article['miaoshu'];},'wimg'=>function(){return $this->_article['wimg'];},'article_created_at'=>function(){return $this->_article['created_at'];},"collection_created_at"=>"created_at",
+            'id','article_id'=>function(){return $this->_article['id'];},'title'=>function(){return $this->_article['title'];},'miaoshu'=>function(){return $this->_article['miaoshu'];},'wimg'=>function(){return $this->_article['wimg'];},'article_created_at'=>function(){return $this->_article['created_at'];},"collection_created_at"=>"created_at",'url'=>function(){return "http://120.27.226.102:82/article/article/show?id=".$this->aid."&uid=".$this->userid;},'like_count'=>function(){return count($this->_like);},'islike'=>function(){return $this->_islike?1:0;},
         ];
     }
 }
