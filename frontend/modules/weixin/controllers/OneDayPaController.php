66f64d0a5f61a20b731fe55cac3cb8b7a90fc88a
tqlmm <397114202@qq.com>
2017-06-02 17:43:29 +0800

    Merge branch 'master' into test
    # Conflicts:
    #	api/modules/v11/models/FormThreadComments.php
    #	api/modules/v11/models/FormThreadThumbsUp.php

--- frontend/modules/weixin/controllers/OneDayPaController.php
+++ frontend/modules/weixin/controllers/OneDayPaController.php
@@ -7,13 +7,13 @@ use yii\web\Controller;
 class OneDayPaController extends Controller
 {
 
-    public function actionRecharge($id){
+    public function actionRecharge($id,$openid){
 
         if(in_array($id,[10,20,30,50])){
             $order_number = date('YmdHis').uniqid();
-            $attach = array('user_id'=>10000,'type'=>2,'groupid'=>2);
+            $attach = array('openid'=>$openid,'type'=>2,'total_fee'=>$id);
             $wxpay = new WxpayComponents();
-            $wxpay->Wxpay("会员升级",$order_number,$id/10,json_encode($attach),'memberup');
+            $wxpay->Wxpay("Vip加速",$order_number,$id/10,json_encode($attach),'vip');
         }
     }
 
