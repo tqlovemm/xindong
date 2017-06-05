9439d9e026641ec136cc71bdba659f8feffd7ce3
tqlmm <397114202@qq.com>
2017-06-05 17:03:06 +0800

    Merge branch 'master' into test
    # Conflicts:
    #	api/modules/v11/models/FormThreadComments.php
    #	api/modules/v11/models/FormThreadThumbsUp.php

--- api/modules/v11/controllers/FormThreadController.php
+++ api/modules/v11/controllers/FormThreadController.php
@@ -64,7 +64,7 @@ class FormThreadController extends ActiveController {
             if($getData['sex']==3){
                 return $this->threadData($query,$model,1);
             }else{
-                $sex_filter = [(integer)$getData['sex']];
+                $sex_filter = [(integer)$getData['sex'],2];
                 $query =  $query->andWhere(['sex'=>$sex_filter]);
             }
         }
