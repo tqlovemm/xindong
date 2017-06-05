6d9679220384c1990758d39f4edbc2e768dbaa02
tqlmm <397114202@qq.com>
2017-06-05 12:17:29 +0800

    Merge branch 'master' into test
    # Conflicts:
    #	api/modules/v11/models/FormThreadComments.php
    #	api/modules/v11/models/FormThreadThumbsUp.php

--- vendor/yiisoft/yii2/wxpay/lib/WxPayNotify.php
+++ vendor/yiisoft/yii2/wxpay/lib/WxPayNotify.php
@@ -68,7 +68,7 @@ class WxPayNotify extends WxPayNotifyReply
                 $payoAccach = ['out_trade_no'=>$data['out_trade_no'],'openid'=>$attach_access['oid'],'total_fee'=>$attach_access['total_fee'],'up_score'=>$attach_access['up_score']];
                 $url = "http://51payo.tecclub.cn/weixin/one-day-pa/record";
                  if((new AccessToken())->postData($url,$payoAccach)){
-                    return true;
+                    return "SUCCESS";
                 }else{
                     return false;
                  }
