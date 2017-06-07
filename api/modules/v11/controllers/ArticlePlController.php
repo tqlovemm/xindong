50af412026cd2c7733476938281201847d7d7a65
xindong <1176306824@qq.com>
2017-06-07 11:02:57 +0800

    article

--- api/modules/v11/controllers/ArticlePlController.php
+++ api/modules/v11/controllers/ArticlePlController.php
@@ -30,6 +30,34 @@ class ArticlePlController extends ActiveController {
         return $action;
     }
 
+    public function actionIndex()
+    {
+        header("Access-Control-Allow-Origin:http://api.13loveme.com:82");
+        $aid = intval($_GET['aid']);
+        if(empty($aid)){
+            Response::show('202','not','not data!');
+        }
+        $res = (new Query())->select('c.id,c.content,c.created_at,u.username,u.nickname,u.avatar')->from('{{%article_comment}} AS c')->leftJoin('{{%user}} AS u','u.id = c.created_id')->limit(1)->orderby('created_at desc')->where("aid = $aid")->all();
+        $time = time();
+        for($i=0;$i<count($res);$i++){
+            if(!$res[$i]['nickname']){
+                $res[$i]['nickname'] = $res[$i]['username'];
+            }
+            $res[$i]['time'] = $time-$res[$i]['created_at'];
+            if($res[$i]['time'] >= 24*3600){
+                $res[$i]['time'] = date('Y-m-d',$res[$i]['created_at']);
+            }else{
+                if($res[$i]['time'] >= 3600){
+                    $res[$i]['time'] = floor($res[$i]['time']/3600)."小时前";
+                }elseif($res[$i]['time'] < 60){
+                    $res[$i]['time'] = "刚刚";
+                }else{
+                    $res[$i]['time'] = floor($res[$i]['time']/60)."分钟前";
+                }
+            }
+        }
+        return Response::show('200','yes',$res);;
+    }
 
     public function actionCreate()
     {
