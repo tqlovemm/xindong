7a9c5e4b05c07bd9745cc6b45b8457eab3f82be6
xindong <1176306824@qq.com>
2017-06-09 11:29:44 +0800

    article

---
+++ backend/web/css/article/main.js
@@ -0,0 +1,94 @@
+/*global Qiniu */
+/*global plupload */
+/*global FileProgress */
+/*global hljs */
+
+
+$(function() {
+    var uploader = Qiniu.uploader({
+        runtimes: 'html5,flash,html4',
+        browse_button: 'pickfiles',
+        container: 'container',
+        drop_element: 'container',
+        max_file_size: '1000mb',
+        flash_swf_url: 'bower_components/plupload/js/Moxie.swf',
+        dragdrop: true,
+        chunk_size: '4mb',
+        multi_selection: false,
+        uptoken: 'h6FGdX9dtB_PN8IvFgsvDBShKLUJAnPTpFDRj9ns:XZcJw1OdRBTYhetKo6srFLT9gb0=:eyJzY29wZSI6ImFwcGltYWdlcyIsImRlYWRsaW5lIjoxNDk2OTEzMDAxLCJ1cEhvc3RzIjpbImh0dHA6XC9cL3VwLnFpbml1LmNvbSIsImh0dHA6XC9cL3VwbG9hZC5xaW5pdS5jb20iLCItSCB1cC5xaW5pdS5jb20gaHR0cDpcL1wvMTgzLjEzNi4xMzkuMTYiXX0=',
+        // uptoken_func: function(){
+        //     var ajax = new XMLHttpRequest();
+        //     ajax.open('GET', $('#uptoken_url').val(), false);
+        //     ajax.setRequestHeader("If-Modified-Since", "0");
+        //     ajax.send();
+        //     if (ajax.status === 200) {
+        //         var res = JSON.parse(ajax.responseText);
+        //         console.log('custom uptoken_func:' + res.uptoken);
+        //         return res.uptoken;
+        //     } else {
+        //         console.log('custom uptoken_func err');
+        //         return '';
+        //     }
+        // },
+        domain: "http://omsnqyd5g.bkt.clouddn.com/",
+        get_new_uptoken: false,
+        // downtoken_url: '/downtoken',
+        // unique_names: true,
+        // save_key: true,
+        // x_vars: {
+        //     'id': '1234',
+        //     'time': function(up, file) {
+        //         var time = (new Date()).getTime();
+        //         // do something with 'time'
+        //         return time;
+        //     },
+        // },
+        auto_start: true,
+        log_level: 5,
+        init: {
+            'FilesAdded': function(up, files) {
+                $('table').show();
+                $('#success').hide();
+                plupload.each(files, function(file) {
+                    var progress = new FileProgress(file, 'fsUploadProgress');
+                    progress.setStatus("等待...");
+                    progress.bindUploadCancel(up);
+                });
+            },
+            'BeforeUpload': function(up, file) {
+                var progress = new FileProgress(file, 'fsUploadProgress');
+                var chunk_size = plupload.parseSize(this.getOption('chunk_size'));
+                if (up.runtime === 'html5' && chunk_size) {
+                    progress.setChunkProgess(chunk_size);
+                }
+            },
+            'UploadProgress': function(up, file) {
+                var progress = new FileProgress(file, 'fsUploadProgress');
+                var chunk_size = plupload.parseSize(this.getOption('chunk_size'));
+                progress.setProgress(file.percent + "%", file.speed, chunk_size);
+            },
+            'UploadComplete': function() {
+                $('#success').show();
+            },
+            'FileUploaded': function(up, file, info) {
+                var progress = new FileProgress(file, 'fsUploadProgress');
+                progress.setComplete(up, info);
+            },
+            'Error': function(up, err, errTip) {
+                $('table').show();
+                var progress = new FileProgress(err.file, 'fsUploadProgress');
+                progress.setError();
+                progress.setStatus(errTip);
+            }
+                // ,
+                // 'Key': function(up, file) {
+                //     var key = "";
+                //     // do something with key
+                //     return key
+                // }
+        }
+    });
+
+    
+
+});
