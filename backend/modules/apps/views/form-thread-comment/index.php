845c22b99e6a44c16fd400e18e04651ae56fbfe7
tqlmm <397114202@qq.com>
2017-06-14 11:31:23 +0800

    fcnh

--- backend/modules/apps/views/form-thread-comment/index.php
+++ backend/modules/apps/views/form-thread-comment/index.php
@@ -13,11 +13,6 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="form-thread-comments-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a('Create Form Thread Comments', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
