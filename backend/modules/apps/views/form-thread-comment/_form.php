3e1e605666c82fe40f4ee012e5b10b89eabbb02c
tqlmm <397114202@qq.com>
2017-06-14 11:28:56 +0800

    fcnh

--- backend/modules/apps/views/form-thread-comment/_form.php
+++ backend/modules/apps/views/form-thread-comment/_form.php
@@ -12,19 +12,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'thread_id')->textInput() ?>
-
-    <?= $form->field($model, 'first_id')->textInput() ?>
-
-    <?= $form->field($model, 'second_id')->textInput() ?>
-
     <?= $form->field($model, 'comment')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'flag')->textInput() ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
