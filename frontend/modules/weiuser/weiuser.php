b01f8b3377f71bc980f77abf007fce2fcbfb8bf1
tqlmm <397114202@qq.com>
2017-06-29 12:02:16 +0800

    噶尔

---
+++ frontend/modules/weiuser/weiuser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace frontend\modules\weiuser;
+
+class weiuser extends \yii\base\Module
+{
+    public $controllerNamespace = 'frontend\modules\weiuser\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
