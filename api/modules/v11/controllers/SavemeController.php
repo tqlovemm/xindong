8cfbcd7f0448baacfc866ee9be54e037d4cec90b
xindong <1176306824@qq.com>
2017-06-29 17:34:09 +0800

    saveme

--- api/modules/v11/controllers/SavemeController.php
+++ api/modules/v11/controllers/SavemeController.php
@@ -60,7 +60,16 @@ class SavemeController extends ActiveController {
         ]);
         $maxpage = ceil($pagination->totalCount/$pagination->defaultPageSize);
         $applyres = (new Query())->select('saveme_id')->from('{{%saveme_apply}}')->where(['apply_uid'=>$uid])->orderBy('created_at desc')->all();
-        $savemeres = $query->orderBy('end_time desc')->where($where)->offset($pagination->offset)->limit($pagination->limit)->all();
+        $savemeres = $query->orderBy('created_at desc')->where($where)->offset($pagination->offset)->limit($pagination->limit)->all();
+        for ($i=1; $i < count($savemeres); $i++) {
+            for ($k=0; $k < count($savemeres)-$i; $k++) {
+                if($savemeres[$k]['end_time'] < $time) {
+                    $tmp=$savemeres[$k+1];
+                    $savemeres[$k+1]=$savemeres[$k];
+                    $savemeres[$k]=$tmp;
+                }
+            }
+        }
         for ($i=0; $i < count($savemeres); $i++) { 
             if ($savemeres[$i]['end_time'] < $time) {
                 $savemeres[$i]['status'] = 3;//已过期
