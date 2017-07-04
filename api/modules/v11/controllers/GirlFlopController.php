22973057f054c622ab1f1ee2b00128499b7b9682
xindong <1176306824@qq.com>
2017-07-04 15:40:38 +0800

    flop

--- api/modules/v11/controllers/GirlFlopController.php
+++ api/modules/v11/controllers/GirlFlopController.php
@@ -165,11 +165,16 @@ class GirlFlopController extends ActiveController {
         //推送
         $user = User2::find()->where(['id'=>$flop_userid])->one();
         if($user['groupid'] == 1 && $user['cid'] && $flop_type == 1){
+            $girluser = User2::find()->where(['id'=>$id])->one();
             $cid = $user['cid'];
             $title = "有女生喜欢你，成为会员可以收到好友通知哦。";
             $msg = "有女生喜欢你，成为会员可以收到好友通知哦。";
             $date = time();
-            $icon = Yii::$app->params['icon'].'/images/app_push/u=3453872033,2552982116&fm=21&gp=0.png';
+            if($girluser['avatar']){
+                $icon = $girluser['avatar'];
+            }else{
+                $icon = '';
+            }
             $extras = json_encode(array('push_title'=>urlencode($title),'push_user_id'=>$id,'push_content'=>urlencode($msg),'push_type'=>'SSCOMM_LIKE_FLOP'));
             Yii::$app->db->createCommand("insert into {{%app_push}} (type,status,cid,title,msg,extras,platform,response,icon,created_at,updated_at) values('SSCOMM_LIKE_FLOP',2,'$cid','$title','$msg','$extras','all','NULL','$icon',$date,$date)")->execute();
             Response::show('200','翻牌成功',"翻牌成功");
