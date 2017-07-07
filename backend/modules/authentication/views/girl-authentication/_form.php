1884d10e15e24c7e0eb33d45a01117113cdb3a5d
xindong <1176306824@qq.com>
2017-07-07 14:46:46 +0800

    flop

--- backend/modules/authentication/views/girl-authentication/_form.php
+++ backend/modules/authentication/views/girl-authentication/_form.php
@@ -23,7 +23,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'status')->textInput()->dropDownList([1=>'认证成功',2=>'认证失败',3=>'未认证']) ?>
     <?php if($update == 1){ ?>
         <div class="form-group">
-            <label class="control-label">备注</label>
+            <label class="control-label">备注 (认证失败填写理由，认证成功不用填写。PS：认证失败也可以不写，将发送默认提醒)</label>
             <input type="text" class="form-control" name="beizhu">
 
             <div class="help-block"></div>
