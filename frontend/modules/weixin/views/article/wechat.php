00adaaa5742f37b962cf60791b18859a59776453
tqlmm <397114202@qq.com>
2017-07-11 16:59:42 +0800

    噶尔

--- frontend/modules/weixin/views/article/wechat.php
+++ frontend/modules/weixin/views/article/wechat.php
@@ -7,134 +7,10 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="format-detection" content="telephone=no">
 
-
-    <script nonce="1706366202" type="text/javascript">
-        window.logs = {
-            pagetime: {}
-        };
-        window.logs.pagetime['html_begin'] = (+new Date());
-    </script>
-
-    <script nonce="1706366202" type="text/javascript">
-        var biz = "MzAxMDIwMDIxMw==" || "";
-        var sn = "" || "" || "e2070920543b6584a325cb28aa1761b8";
-        var mid = "503683819" || "" || "503683819";
-        var idx = "1" || "" || "1";
-        window.__allowLoadResFromMp = true;
-
-    </script>
-    <script nonce="1706366202" type="text/javascript">
-        var page_begintime = +new Date, is_rumor = "", norumor = "";
-        1 * is_rumor && !(1 * norumor) && biz && mid && (document.referrer && -1 != document.referrer.indexOf("mp.weixin.qq.com/mp/rumor") || (location.href = "http://mp.weixin.qq.com/mp/rumor?action=info&__biz=" + biz + "&mid=" + mid + "&idx=" + idx + "&sn=" + sn + "#wechat_redirect")),
-            document.domain = "qq.com";
-    </script>
-    <script nonce="1706366202" type="text/javascript">
-        var MutationObserver = window.WebKitMutationObserver || window.MutationObserver || window.MozMutationObserver, isDangerSrc = function (t) {
-            if (t) {
-                var e = t.match(/http(?:s)?:\/\/([^\/]+?)(\/|$)/);
-                if (e && !/qq\.com(\:8080)?$/.test(e[1]) && !/weishi\.com$/.test(e[1]))return !0;
-            }
-            return !1;
-        }, ishttp = 0 == location.href.indexOf("http://");
-        -1 == location.href.indexOf("safe=0") && ishttp && "function" == typeof MutationObserver && "mp.weixin.qq.com" == location.host && (window.__observer_data = {
-            count: 0,
-            exec_time: 0,
-            list: []
-        }, window.__observer = new MutationObserver(function (t) {
-            window.__observer_data.count++;
-            var e = new Date, r = [];
-            t.forEach(function (t) {
-                for (var e = t.addedNodes, o = 0; o < e.length; o++) {
-                    var n = e[o];
-                    if ("SCRIPT" === n.tagName) {
-                        var i = n.src;
-                        isDangerSrc(i) && (window.__observer_data.list.push(i), r.push(n)), !i && window.__nonce_str && n.getAttribute("nonce") != window.__nonce_str && (window.__observer_data.list.push("inlinescript_without_nonce"),
-                            r.push(n));
-                    }
-                }
-            });
-            for (var o = 0; o < r.length; o++) {
-                var n = r[o];
-                n.parentNode && n.parentNode.removeChild(n);
-            }
-            window.__observer_data.exec_time += new Date - e;
-        }), window.__observer.observe(document, {
-            subtree: !0,
-            childList: !0
-        })), function () {
-            if (-1 == location.href.indexOf("safe=0") && Math.random() < .01 && ishttp && HTMLScriptElement.prototype.__lookupSetter__ && "undefined" != typeof Object.defineProperty) {
-                window.__danger_src = {
-                    xmlhttprequest: [],
-                    script_src: [],
-                    script_setAttribute: []
-                };
-                var t = "$" + Math.random();
-                HTMLScriptElement.prototype.__old_method_script_src = HTMLScriptElement.prototype.__lookupSetter__("src"),
-                    HTMLScriptElement.prototype.__defineSetter__("src", function (t) {
-                        t && isDangerSrc(t) && window.__danger_src.script_src.push(t), this.__old_method_script_src(t);
-                    });
-                var e = "element_setAttribute" + t;
-                Object.defineProperty(Element.prototype, e, {
-                    value: Element.prototype.setAttribute,
-                    enumerable: !1
-                }), Element.prototype.setAttribute = function (t, r) {
-                    "SCRIPT" == this.tagName && "src" == t && isDangerSrc(r) && window.__danger_src.script_setAttribute.push(r),
-                        this[e](t, r);
-                };
-            }
-        }();
-    </script>
-
     <link rel="dns-prefetch" href="//res.wx.qq.com">
     <link rel="dns-prefetch" href="//mmbiz.qpic.cn">
     <link rel="shortcut icon" type="image/x-icon"
           href="//res.wx.qq.com/mmbizwap/zh_CN/htmledition/images/icon/common/favicon22c41b.ico">
-    <script nonce="1706366202" type="text/javascript">
-        String.prototype.html = function (encode) {
-            var replace = ["&#39;", "'", "&quot;", '"', "&nbsp;", " ", "&gt;", ">", "&lt;", "<", "&amp;", "&", "&yen;", "¥"];
-            if (encode) {
-                replace.reverse();
-            }
-            for (var i = 0, str = this; i < replace.length; i += 2) {
-                str = str.replace(new RegExp(replace[i], 'g'), replace[i + 1]);
-            }
-            return str;
-        };
-
-        window.isInWeixinApp = function () {
-            return /MicroMessenger/.test(navigator.userAgent);
-        };
-
-        window.getQueryFromURL = function (url) {
-            url = url || 'http://qq.com/s?a=b#rd';
-            var tmp = url.split('?'),
-                query = (tmp[1] || "").split('#')[0].split('&'),
-                params = {};
-            for (var i = 0; i < query.length; i++) {
-                var arg = query[i].split('=');
-                params[arg[0]] = arg[1];
-            }
-            if (params['pass_ticket']) {
-                params['pass_ticket'] = encodeURIComponent(params['pass_ticket'].html(false).html(false).replace(/\s/g, "+"));
-            }
-            return params;
-        };
-
-        (function () {
-            var params = getQueryFromURL(location.href);
-            window.uin = params['uin'] || "" || '';
-            window.key = params['key'] || "" || '';
-            window.wxtoken = params['wxtoken'] || '';
-            window.pass_ticket = params['pass_ticket'] || '';
-        })();
-
-        function wx_loaderror() {
-            if (location.pathname === '/bizmall/reward') {
-                new Image().src = '/mp/jsreport?key=96&content=reward_res_load_err&r=' + Math.random();
-            }
-        }
-
-    </script>
 
     <title>心动三十一天</title>
 
@@ -4996,28 +4872,6 @@
                     <p><img src="http://oqfwt261i.bkt.clouddn.com/weixin/sucaitu/weiqun.jpg"></p>
                     <p><br></p>
                 </div>
-                <script nonce="1706366202" type="text/javascript">
-                    var first_sceen__time = (+new Date());
-
-                    if ("" == 1 && document.getElementById('js_content')) {
-                        document.getElementById('js_content').addEventListener("selectstart", function (e) {
-                            e.preventDefault();
-                        });
-                    }
-
-
-                    (function () {
-                        if (navigator.userAgent.indexOf("WindowsWechat") != -1) {
-                            var link = document.createElement('link');
-                            var head = document.getElementsByTagName('head')[0];
-                            link.rel = 'stylesheet';
-                            link.type = 'text/css';
-                            link.href = "//res.wx.qq.com/mmbizwap/zh_CN/htmledition/style/page/appmsg/page_mp_article_improve_winwx31619e.css";
-                            head.appendChild(link);
-                        }
-                    })();
-                </script>
-
 
                 <div class="ct_mpda_wrp" id="js_sponsor_ad_area" style="display:none;"></div>
 
