244fd2503b560891780c9bbd6251114a60ddb484
tqlmm <397114202@qq.com>
2017-07-11 12:54:36 +0800

    噶尔

--- api/modules/v11/controllers/GirlFlopController.php
+++ api/modules/v11/controllers/GirlFlopController.php
@@ -229,7 +229,7 @@ class GirlFlopController extends ActiveController {
 
             $newarr = array();
             foreach ($boysres as $key=>$boysr){
-                var_dump($boysr['address']);
+                var_dump($boysr->address);
                 var_dump($boysr);
 
                 return;
