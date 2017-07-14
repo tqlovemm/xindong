1b00ee2205c3d6847ca87bf3bd6b8733479b1751
tqlmm <397114202@qq.com>
2017-07-14 17:38:47 +0800

    噶尔

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -7,25 +7,19 @@ use yii\grid\GridView;
 /* @var $searchModel backend\models\UserSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = Yii::t('app', 'Users');
+$this->title = "所有注册会员";
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
     <p>
-        <?= Html::a(Yii::t('app', 'Create {modelClass}', [
-    'modelClass' => 'User',
-]), ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Yii::t('app', 'Create {modelClass}', ['modelClass' => 'User',]), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
-
             ['class' => 'yii\grid\ActionColumn'],
             [
                 'attribute' => 'number',
@@ -34,52 +28,27 @@ $this->params['breadcrumbs'][] = $this->title;
                     $user = \backend\models\User::getNumber($data->id);
                     return $user;
                 }
-
             ],
             'id',
             'cid',
             'username',
             'nickname',
-            //'password_hash',
-           //'password_reset_token',
             [
                 'attribute' => 'groupid',
                 'label' => '会员等级',
                 'value' => function ($data) {
                     return \common\components\Vip::vip($data->groupid);
                 }
-
             ],
-            //'identify',
             'cellphone',
             'email:email',
-            //'status',
-            //'role',
             [
                 'attribute' => 'sex',
                 'label' => '性别',
                 'value' => function ($data) {
-                    if($data->sex==0){
-                        $vip = "0-男";
-                    }elseif($data->sex==1){
-                        $vip = "1-女";
-                    }else{
-                        $vip = "未知错误";
-                    }
-
-                    return $vip;
+                    return \common\components\Vip::sex($data->sex);
                 }
-
             ],
-            //'salary',
-            //'weibo_num',
-            //'job',
-            //'car',
-            //'cup',
-             //'created_at:datetime',
-             //'updated_at:datetime',
-             //'avatar',
-
         ],
     ]); ?>
 
