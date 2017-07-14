321a271010dcb7148d85b1ea9158b53d175c46b4
tqlmm <397114202@qq.com>
2017-07-14 11:29:47 +0800

    噶尔

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -16,7 +16,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'username')->textInput(['maxlength' => 32,'disabled'=>true]) ?>
     <?= $form->field($model, 'email')->textInput(['maxlength' => 64]) ?>
     <?= $form->field($model, 'cellphone')->textInput() ?>
-    <?= $form->field($model, 'sex')->dropDownList([0=>'男生',10=>'女生'])->label('性别') ?>
+    <?= $form->field($model, 'sex')->dropDownList([0=>'男生',1=>'女生'])->label('性别') ?>
     <?= $form->field($model, 'status')->dropDownList([0=>'封号',10=>'开通'])->label('是否封号，如果封号网站和APP将同时无法登陆') ?>
 
     <label>用户头像</label>
