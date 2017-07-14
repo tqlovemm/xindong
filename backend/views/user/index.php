3bfc64142177263f601d2f21962da3c6099cbc36
tqlmm <397114202@qq.com>
2017-07-14 18:04:22 +0800

    噶尔

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -11,7 +11,7 @@ $this->title = "所有注册会员";
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-index">
-    
+
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
@@ -34,7 +34,15 @@ $this->params['breadcrumbs'][] = $this->title;
                 'label' => '会员等级',
                 'value' => function ($data) {
                     return Vip::vip($data->groupid);
-                }
+                },
+                'filter' => [
+                    0 => '网站会员',
+                    1 => '包月会员',
+                    2 => '普通会员',
+                    3 => '高端会员',
+                    4 => '至尊会员',
+                    5 => '私人定制',
+                ]
             ],
             'cellphone',
             'email:email',
@@ -43,7 +51,11 @@ $this->params['breadcrumbs'][] = $this->title;
                 'label' => '性别',
                 'value' => function ($data) {
                     return Vip::sex($data->sex);
-                }
+                },
+                'filter' => [
+                    0 => '男',
+                    1 => '女'
+                ]
             ],
         ],
     ]); ?>
